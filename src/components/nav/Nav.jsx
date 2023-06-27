4171e014458bb9a7d6e9503334cb636348b36542
Z-100 <marvin131t@gmail.com>
2023-06-27 11:34:34 +0200

    feature(structure): add disclaimer to nav

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import {
+  React,
+  useState
+} from 'react'
 import './nav.css'
 import {AiOutlineHome} from 'react-icons/ai'
 import {AiOutlineUser} from 'react-icons/ai'
-import {BiBook} from 'react-icons/bi'
+import {
+  BiMessageSquareDetail,
+  BiBook,
+  BiCopyright,
+  BiInfoCircle
+} from 'react-icons/bi'
 import {RiServiceLine} from 'react-icons/ri'
-import {BiMessageSquareDetail} from 'react-icons/bi'
-import {useState} from 'react'
 
 const Nav = () => {
   const [activeNav, setActiveNav] = useState('')
@@ -14,11 +20,11 @@ const Nav = () => {
       <a href="home" onClick={() => setActiveNav('home')} className={activeNav === 'home' ? 'active' : ''}><AiOutlineHome/></a>
       <a href="aboutus" onClick={() => setActiveNav('aboutus')} className={activeNav === 'aboutus' ? 'active' : ''}><AiOutlineUser/></a>
         <a href="videotutorials" onClick={() => setActiveNav('videotutorials')} className={activeNav === 'videotutorials' ? 'active' : ''}><BiBook/></a>
-        <a href="copyright" onClick={() => setActiveNav('copyright')} className={activeNav === 'copyright' ? 'active' : ''}><BiBook/></a>
         <a href="softwaretools" onClick={() => setActiveNav('softwaretools')} className={activeNav === 'softwaretools' ? 'active' : ''}><RiServiceLine/></a>
         <a href="formatting" onClick={() => setActiveNav('formatting')} className={activeNav === 'formatting' ? 'active' : ''}><BiMessageSquareDetail/></a>
 
-
+        <a href="copyright" onClick={() => setActiveNav('copyright')} className={activeNav === 'copyright' ? 'active' : ''}><BiCopyright/></a>
+        <a href="disclaimer" onClick={() => setActiveNav('disclaimer')} className={activeNav === 'disclaimer' ? 'active' : ''}><BiInfoCircle/></a>
     </nav>
   )
 }
