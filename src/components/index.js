441c2284b00529f019b6c25774ac2e93f2cb793c
Z-100 <marvin131t@gmail.com>
2023-06-29 16:58:38 +0200

    feature(structure): Refactor

--- src/components/index.js
+++ src/components/index.js
@@ -1,13 +1,20 @@
-export {default as About} from './about/About'
 export {default as ArticleTexts} from './common/ArticleTexts'
 export {default as ImageCards} from './common/ImageCards'
-export {default as Contact} from './contact/Contact'
-export {default as Experience} from './experience/Experience'
+export {default as TeamShowcase} from './team/TeamShowcase'
+
 export {default as Footer} from './footer/Footer'
+export {default as Nav} from './nav/Nav'
+
+
+
+
+
+export {default as Contact} from './contact/Contact'
+export {default as About} from './about/About'
+export {default as Experience} from './experience/Experience'
 export {default as CTA} from './header/CTA'
 export {default as Header} from './header/Header'
 export {default as HeaderSocials} from './header/HeaderSocials'
-export {default as Nav} from './nav/Nav'
 export {default as Portfolio} from './portfolio/Portfolio'
 export {default as SoftwareTools} from './softwaretools/SoftwareTools'
 export {default as Testimonials} from './testimonials/Testimonials'
