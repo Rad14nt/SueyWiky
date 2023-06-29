49ec7eb310ec6cad2e8832e5488fba2093f23b10
Z-100 <marvin131t@gmail.com>
2023-06-29 17:10:30 +0200

    feature(structure): Refactor to use function

--- src/components/common/ArticleTexts.jsx
+++ src/components/common/ArticleTexts.jsx
@@ -1,6 +1,7 @@
 import './textbox.css'
 import {VscFolderLibrary} from 'react-icons/vsc'
 import {Link} from "react-router-dom";
+import {StringToMultiline} from "../../functions";
 
 const ArticleTexts = (props) => {
 
@@ -34,9 +35,7 @@ const ArticleTexts = (props) => {
                         {content.subjects.map((subject, index) => (
                             <div key={index} className="" id={subject.subTitle.replace(' ', '-').toLowerCase()}>
                                 <h3>{subject.subTitle}</h3>
-                                {subject.subTextLong.split('\n').map((line, index) =>
-                                    <p key={index}>{line}</p>
-                                )}
+                                <StringToMultiline text={subject.subTextLong} />
                             </div>
                         ))}
                     </div>
